a995da57dbf6c060fd89121c93eff69079483e9c
weekly123 <weekly123@gmial.com>
2014-11-06 16:10:46 +0800

    delete commet

--- ejb-demo-client/src/org/xmwu/demo/client/HelloClient.java
+++ ejb-demo-client/src/org/xmwu/demo/client/HelloClient.java
@@ -11,7 +11,6 @@
  */
 package org.xmwu.demo.client;
 
-
 import java.util.Properties;
 
 import javax.naming.Context;
@@ -19,7 +18,8 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.xmwu.demo.Hello;
- /**
+
+/**
  * ClassName: HelloClient <br/>
  * Description: TODO <br/>
  * Date: 2014年11月5日 下午1:38:56 <br/>
@@ -27,7 +27,7 @@ import org.xmwu.demo.Hello;
  * 
  * @author xmwu(邮箱)
  * 
- * 修改记录
+ *         修改记录
  * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息<br/>
  * 
  */
@@ -37,19 +37,11 @@ public class HelloClient {
         final Properties env = new Properties();
         env.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
         env.put("jboss.naming.client.ejb.context", true);
-//        env.put("remote.connectionprovider.create.options.org.xnio.Options.SSL_ENABLED", "false");
-//        env.put("remote.connections","default");
-//        env.put("remote.connection.default.host", "localhost");
-//        env.put("remote.connection.default.port", "4447");
-        
         InitialContext ic = new InitialContext(env);
         Hello sless = (Hello) ic.lookup("ejb:/ejb-demo//HelloBean!org.xmwu.demo.Hello");
-        //Hello sless = (Hello) ic.lookup("java:module/HelloBean");
-        System.out.println(sless.add(1,1));
+        System.out.println(sless.add(1, 1));
         System.out.println(sless.hello("xiaoming"));
-        //System.out.println("returnMessage():" + returnMessage());
-        //System.out.println("StatelessSession bean says : " + returnMessage());
+
     }
 
-   
 }
